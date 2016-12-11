raidancampbell/sequelspeare

commit 55e836b58d588424335b73ad7d5485f163dbcfa1

updated command line descriptions

sequelspeare.py
@@ -122,8 +122,8 @@ class StupidSpeare(irc.bot.SingleServerIRCBot):
 
 # parse args from command line invocation
 def parse_args():
-    parser = argparse.ArgumentParser(description="runs a stupider version of the late-great swiggityspeare IRC bot")
-    parser.add_argument('--json_filename', type=str, help="Filename of the json configuration file [stupidspeare.json]",
+    parser = argparse.ArgumentParser(description="runs the sequelspeare IRC bot")
+    parser.add_argument('--json_filename', type=str, help="Filename of the json configuration file [sequelspeare.json]",
                         required=False)
     return parser.parse_args()
 
@@ -132,5 +132,6 @@ def parse_args():
 if __name__ == '__main__':
     args = parse_args()
     json_filename_ = args.json_filename or 'sequelspeare.json'
-    bot = StupidSpeare(json_filename=json_filename_)
+    bot = 
+    Speare(json_filename=json_filename_)
     bot.start()

commit 0ce176ba5b8a33a5ea4b61e1708f3c2f3430622a

fixed class naming bug introduced by 55e836b58d588424335b73ad7d5485f163dbcfa1

sequelspeare.py
@@ -21,7 +21,7 @@ from sample import Sampler
 from jaraco.stream import buffer
 
 
-class StupidSpeare(irc.bot.SingleServerIRCBot):
+class SequelSpeare(irc.bot.SingleServerIRCBot):
     def __init__(self, json_filename):
         self.json_filename = json_filename
         with open(json_filename, 'r') as infile:
@@ -132,6 +132,5 @@ def parse_args():
 if __name__ == '__main__':
     args = parse_args()
     json_filename_ = args.json_filename or 'sequelspeare.json'
-    bot = 
-    Speare(json_filename=json_filename_)
+    bot = SequelSpeare(json_filename=json_filename_)
     bot.start()