7e632e450730db42c08248526c49b157373e005e
R. Aidan Campbell <aidan.campbell@gmail.com>
2016-12-13 21:01:15 -0500

    Generate a summary .csv of all the training metadata files

--- plot_data.py
+++ plot_data.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import glob
+import statistics
 
 
 class Plotter:
@@ -16,46 +17,51 @@ class Plotter:
 
     @staticmethod
     def analyze(should_plot, visibly_plot=False):
-        filenames = glob.glob("training_metadata/*.csv")
+        filenames = glob.glob("training_metadata/training_*.csv")
         total_time = 0.
-        for filenum, filename in enumerate(filenames):
-            with open(filename) as csvfile:
-                reader = csv.reader(csvfile, delimiter=',')
-                errors = []
-                for i, row in enumerate(reader):
-                    if not i:
-                        continue  # first row contains the header string
-                    errors.append(float(row[0]))
-                    total_time += float(row[1])
-                if not should_plot:
-                    continue
-                if errors:
-                    for i, substr_ in enumerate(filename[:-4].split('_')):
+        with open('training_metadata/metadata_sum.csv', 'w') as summary_csv:
+            summary_writer = csv.writer(summary_csv)
+            for filenum, filename in enumerate(filenames):
+                with open(filename, 'r') as csvfile:
+                    reader = csv.reader(csvfile, delimiter=',')
+                    errors = []
+                    trial_time = 0
+                    for i, row in enumerate(reader):
                         if not i:
-                            continue
-                        if 'x' in substr_:
-                            width = substr_.split('x')[0]
-                            depth = substr_.split('x')[1]
-                        if 'l' in substr_:
-                            learning_rate = substr_[:-1]
-                        if 'd' in substr_:
-                            decay_rate = substr_[:-1]
-                        if 'e' in substr_:
-                            epochs = substr_[:-1]
-                    if visibly_plot:
-                        plt.figure(filenum+1)
-                    plt.plot(Plotter.moving_average(errors, n=2500))
-                    title = width + ' by ' + depth + ' network. \n''\
-                        learning rate: ' + learning_rate + ' decay rate: ' + decay_rate + ' epochs: ' + epochs
-                    plt.title(title)
-                    plt.savefig(filename[:-4] + '.png')
-                    print('[' + str(filenum) + '/' + str(len(filenames)) + '] Finished analyzing file: ' + filename)
-                    # if we're not viewing the interactive figures, clear the figure to free up memory
-                    if not visibly_plot:
-                        plt.clf()
-        if visibly_plot:
-            plt.show()
-        print('total training time: ' + str(total_time) + ' seconds')
+                            continue  # first row contains the header string
+                        errors.append(float(row[0]))
+                        total_time += float(row[1])
+                        trial_time += float(row[1])
+                    if not should_plot:
+                        continue
+                    if errors:
+                        summary_writer.writerow([filename[27:-4], statistics.mean(errors[-2500:]), trial_time])
+                        for i, substr_ in enumerate(filename[:-4].split('_')):
+                            if not i:
+                                continue
+                            if 'x' in substr_:
+                                width = substr_.split('x')[0]
+                                depth = substr_.split('x')[1]
+                            if 'l' in substr_:
+                                learning_rate = substr_[:-1]
+                            if 'd' in substr_:
+                                decay_rate = substr_[:-1]
+                            if 'e' in substr_:
+                                epochs = substr_[:-1]
+                        if visibly_plot:
+                            plt.figure(filenum+1)
+                        plt.plot(Plotter.moving_average(errors, n=2500))
+                        title = width + ' by ' + depth + ' network. \n''\
+                            learning rate: ' + learning_rate + ' decay rate: ' + decay_rate + ' epochs: ' + epochs
+                        plt.title(title)
+                        plt.savefig(filename[:-4] + '.png')
+                        print('[' + str(filenum) + '/' + str(len(filenames)) + '] Finished analyzing file: ' + filename)
+                        # if we're not viewing the interactive figures, clear the figure to free up memory
+                        if not visibly_plot:
+                            plt.clf()
+            if visibly_plot:
+                plt.show()
+            print('total training time: ' + str(total_time) + ' seconds')
 
 
 if __name__ == '__main__':
