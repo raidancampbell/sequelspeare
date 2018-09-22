ac3fac22febdb6c309ea4c6c4dc95464695b928c
R. Aidan Campbell <aidan.campbell@gmail.com>
2018-09-22 11:17:51 -0700

    Bugfixes on thermal printing to prevent failing and logging on each message if misconfigured

--- Features/Printable.py
+++ Features/Printable.py
@@ -14,11 +14,19 @@ class Printable(AbstractFeature):
         # defer initialization until the first message is seen.
         # This prevents try/catch on initialization, even when this feature is disabled
         self.epson = None
+        self.failed_initialization = False
 
     def message_filter(self, bot, source, target, message, highlighted):
+        if self.failed_initialization:
+            return False
         if not self.epson:
-            self.epson = self.epson or printer.Serial(self.printer_dev or '/dev/ttyUSB0')
-            self.epson.set(align='left', font='b', width=1, height=1, density=9)
+            try:
+                self.epson = self.epson or printer.Serial(self.printer_dev or '/dev/ttyUSB0')
+                self.epson.set(align='left', font='b', width=1, height=1, density=9)
+            except Exception as e:
+                print('failed to initialize printer, silently ignoring')
+                print(e)
+                self.failed_initialization = True
         timestamp = datetime.datetime.now()
         text = '{} {}: {}\n'.format(timestamp.strftime('%m/%d %H:%M:%S '), target, message)
         text = text.replace('\\', '\\\\')
