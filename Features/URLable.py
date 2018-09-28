4eb28a5a339716fe700216e5369903de048454e5
R. Aidan Campbell <aidan.campbell@gmail.com>
2018-09-28 11:18:38 -0700

    URL title fetching is now faster, and times out instead of waiting

--- Features/URLable.py
+++ Features/URLable.py
@@ -1,25 +1,24 @@
+import requests
+
 from Features.AbstractFeature import AbstractFeature
+from contextlib import closing
 import validators
-import urllib.request
-import ssl
 from bs4 import BeautifulSoup
 
 
 class URLable(AbstractFeature):
+    MAX_RECV = 1000000
 
     @staticmethod
     def description():
         return 'Passively retrieves each URL posted, and provides the HTML Title as a summary of the link'
 
-    def __init__(self):
-        self.no_ssl = ssl._create_unverified_context()
-
     def message_filter(self, bot, source, target, message, highlighted):
         url = self.extract_url(message)
         if url:
             try:
-                soup = BeautifulSoup(urllib.request.urlopen(url, context=self.no_ssl), "html.parser")
-                bot.message(source, soup.title.string.split('\n')[0])
+                title = URLable.get_title(url)
+                bot.message(source, title)
             except Exception:
                 pass
         return False
@@ -34,3 +33,21 @@ class URLable(AbstractFeature):
             except Exception:
                 pass
         return False
+
+    @staticmethod
+    def get_title(url):
+        with closing(requests.get(url, stream=True, timeout=3)) as response:
+            response.raise_for_status()
+            content = response.raw.read(URLable.MAX_RECV + 1, decode_content=True)
+            # Sites advertising ISO-8859-1 are often lying
+            if response.encoding == 'ISO-8859-1':
+                response.encoding = "utf-8"
+            encoding = response.encoding
+
+        if len(content) > URLable.MAX_RECV:
+            return
+
+        html = BeautifulSoup(content, "lxml", from_encoding=encoding)
+
+        if html.title:
+            return " ".join(html.title.text.strip().splitlines())
