a379f7fdec6ad49d2099ff31dd6a2796a7f9b351
R. Aidan Campbell <aidan.campbell@gmail.com>
2019-02-11 07:33:37 -0700

    Clean printer code to account for preferences creating on not found

--- Features/Printable.py
+++ Features/Printable.py
@@ -12,7 +12,7 @@ class Printable(AbstractFeature):
         return 'Passively prints all messages the bot sees to an epson thermal printer.'
 
     def __init__(self):
-        self.printer_dev = self._try_read_device()
+        self.printer_dev = prefs_singleton.read_with_default(type(self).__name__ + '_printer_dev', '')
 
         # defer initialization until the first message is seen.
         # This prevents try/catch on initialization, even when this feature is disabled
@@ -26,8 +26,9 @@ class Printable(AbstractFeature):
             try:
                 self.epson = self.epson or printer.Serial(self.printer_dev)
                 self.epson.set(align='left', font='b', width=1, height=1, density=9)
-            except Exception:
+            except Exception as e:
                 print('failed to initialize printer, silently ignoring...')
+                print(e)
                 self.failed_initialization = True
                 return False
         timestamp = datetime.datetime.now()
@@ -35,9 +36,3 @@ class Printable(AbstractFeature):
         text = text.replace('\\', '\\\\')
         self.epson.text(text)
         return False
-
-    def _try_read_device(self):
-        printer_device = prefs_singleton.read_value(type(self).__name__ + '_printer_dev')
-        if not printer_device:
-            prefs_singleton.write_value(type(self).__name__ + '_printer_dev', '')
-        return printer_device
